be236855408a1bae22b514513623e1f55b482272
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-11-25 19:41:21 +0530

    JWT_SETUP

--- app.js
+++ app.js
@@ -1,10 +1,60 @@
 const express = require("express");
-require('dotenv').config();
+require("dotenv").config();
+require("./config/database").connect();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
+const User = require("./model/user");
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello from auth system</h1>");
+});
+
+app.post("/register", async (req, res) => {
+  try{
+    const { firstname, lastname, email, password } = req.body;
+
+    if (!(email && password && firstname && lastname)) {
+      res.status(400).send("All fields are required");
+    }
+  
+    const existingUser = await Users.findOne({ email });
+  
+    if (existingUser) {
+      res.status(401).send("User already Exists");
+    }
+  
+    const myEncPassword = await bcrypt.hash(password, 10);
+  
+    const user = await User.create({
+      firstname,
+      lastname,
+      email: email.toLowerCase(),
+      password: myEncPassword,
+    });
+  
+    //token creation
+  
+    const token = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.SECRET_KEY,
+      {
+        expiresIn: "2h",
+      }
+    );
+  
+    user.token = token;
+    //Update or not in db
+
+    //Todo Handle Password situation
+  
+    res.status(201).json(user); 
+  }catch(err){
+    console.log(err);
+  }
 });
 
 module.exports = app;
