5b108fd89de73acdd316b1ae9a3d35c4c97a19fe
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-11-26 12:32:52 +0530

    COOKIES_SETUP_CUSTOM_MIDDLEWARES

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const express = require("express");
 require("dotenv").config();
 require("./config/database").connect();
+const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-
+var cookieParser = require("cookie-parser");
 const User = require("./model/user");
-const app = express();
+const auth = require("./middleware/auth");
 
+const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello from auth system</h1>");
+  res.send("<h1>Hello from auth system - LCO</h1>");
 });
 
 app.post("/register", async (req, res) => {
@@ -21,10 +23,10 @@ app.post("/register", async (req, res) => {
       res.status(400).send("All fields are required");
     }
 
-    const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({ email }); // PROMISE
 
     if (existingUser) {
-      res.status(401).send("User already Exists");
+      res.status(401).send("User already exists");
     }
 
     const myEncPassword = await bcrypt.hash(password, 10);
@@ -36,27 +38,24 @@ app.post("/register", async (req, res) => {
       password: myEncPassword,
     });
 
-    //token creation
-
+    //token
     const token = jwt.sign(
-      { id: user._id, email: user.email },
+      { user_id: user._id, email },
       process.env.SECRET_KEY,
       {
         expiresIn: "2h",
       }
     );
-
     user.token = token;
-    //Update or not in db
+    //update or not in DB
 
-    //Todo Handle Password situation
+    // handle password situation
     user.password = undefined;
 
-    //send token or send just success yes and redirect - choice
-
+    // send token or send just success yes and redirect - choice
     res.status(201).json(user);
-  } catch (err) {
-    console.log(err);
+  } catch (error) {
+    console.log(error);
   }
 });
 
@@ -65,35 +64,48 @@ app.post("/login", async (req, res) => {
     const { email, password } = req.body;
 
     if (!(email && password)) {
-      res.status(400).send("All fields are required");
+      res.status(400).send("Field is missing");
     }
 
-    const user = await User.findOne({ email: email.toLowerCase() });
+    const user = await User.findOne({ email });
 
-    // if (!user) {
-    //   res.status(400).send("User not found");
+    // if(!user){
+    //   res.status(400).send("You are not registered in our app")
     // }
-    // const isMatch = await bcrypt.compare(password, user.password);
 
     if (user && (await bcrypt.compare(password, user.password))) {
       const token = jwt.sign(
-        { id: user._id, email: user.email },
+        { user_id: user._id, email },
         process.env.SECRET_KEY,
         {
           expiresIn: "2h",
         }
       );
-
       user.token = token;
       user.password = undefined;
+      // res.status(200).json(user);
 
-      res.status(200).json(user);
+      // if you want to use cookies
+      const options = {
+        expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+      };
+
+      res.status(200).cookie("token", token, options).json({
+        success: true,
+        token,
+        user,
+      });
     }
 
-    res.status(400).send("email or password is incorrect");
-  } catch (err) {
-    console.log(err);
+    res.sendStatus(400).send("email or password is incorrect");
+  } catch (error) {
+    console.log(error);
   }
+});
+
+app.get("/dashboard", auth, (req, res) => {
+  res.send("Welcome to secret information");
 });
 
 module.exports = app;
