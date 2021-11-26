5d6945607aaa3b4e1d03d19afe377bc7d88fe951
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-11-26 10:34:13 +0530

    LOGIN_ROUTE_DONE

--- app.js
+++ app.js
@@ -14,30 +14,30 @@ app.get("/", (req, res) => {
 });
 
 app.post("/register", async (req, res) => {
-  try{
+  try {
     const { firstname, lastname, email, password } = req.body;
 
     if (!(email && password && firstname && lastname)) {
       res.status(400).send("All fields are required");
     }
-  
+
     const existingUser = await User.findOne({ email });
-  
+
     if (existingUser) {
       res.status(401).send("User already Exists");
     }
-  
+
     const myEncPassword = await bcrypt.hash(password, 10);
-  
+
     const user = await User.create({
       firstname,
       lastname,
       email: email.toLowerCase(),
       password: myEncPassword,
     });
-  
+
     //token creation
-  
+
     const token = jwt.sign(
       { id: user._id, email: user.email },
       process.env.SECRET_KEY,
@@ -45,16 +45,53 @@ app.post("/register", async (req, res) => {
         expiresIn: "2h",
       }
     );
-  
+
     user.token = token;
     //Update or not in db
 
     //Todo Handle Password situation
     user.password = undefined;
-    
-  
-    res.status(201).json(user); 
-  }catch(err){
+
+    //send token or send just success yes and redirect - choice
+
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!(email && password)) {
+      res.status(400).send("All fields are required");
+    }
+
+    const user = await User.findOne({ email: email.toLowerCase() });
+
+    // if (!user) {
+    //   res.status(400).send("User not found");
+    // }
+    // const isMatch = await bcrypt.compare(password, user.password);
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign(
+        { id: user._id, email: user.email },
+        process.env.SECRET_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+
+      user.token = token;
+      user.password = undefined;
+
+      res.status(200).json(user);
+    }
+
+    res.status(400).send("email or password is incorrect");
+  } catch (err) {
     console.log(err);
   }
 });
